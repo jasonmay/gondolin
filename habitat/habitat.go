d180abddef848e7290fdf0551a2dc108c61419d4
Jason May <jason.a.may@gmail.com>
2017-02-25 19:23:09 -0800

    proof of concept!

--- habitat/habitat.go
+++ habitat/habitat.go
@@ -1,9 +1,31 @@
 package habitat
 
+/*
+Habitat - the habitat in which the culture lives.
+
+A more useful description: this is the hub in which the mobiles,
+objects, and players all get moved around in a whirlwind of channel
+dispatching and processing.
+*/
+
 import (
     "time"
     "gondolin/loader"
 )
+
+func forward(f chan int, n int) {
+    f <- n
+    time.Sleep(time.Second * time.Duration(n))
+    next := n % 5 + 1
+    go forward(f, next)
+}
+
+func backward(b chan int, n int) {
+    b <- n
+    time.Sleep(time.Second * time.Duration(n))
+    next := ((n + 3) % 5) + 1
+    go backward(b, next)
+}
 
 func Run() {
     // fmt.Printf("%v\n", loader.Load())
@@ -11,9 +33,19 @@ func Run() {
     //m := loader.Load()
     // fmt.Printf("%v\n", m.Mob[0].Properties[1].Name)
     // fmt.Printf("%v\n", m.Mob[0].Properties[1].Value)
-    println("Loaded culture from JSON")
 
-    timer := time.NewTimer(time.Second * 2)
-    <- timer.C
-    println("Timer expired")
+    f := make(chan int)
+    b := make(chan int)
+
+    go forward(f, 1)
+    go backward(b, 1)
+
+    for {
+        select {
+        case n := <-f:
+            println("forward to ", n)
+        case n := <-b:
+            println("backward to ", n)
+        }
+    }
 }
