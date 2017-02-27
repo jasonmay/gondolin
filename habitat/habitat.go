873bcedade081a21be1ce8e7b88ef912955a8338
Jason May <jason.a.may@gmail.com>
2017-02-26 19:46:16 -0800

    comments

--- habitat/habitat.go
+++ habitat/habitat.go
@@ -1,12 +1,8 @@
+//Habitat - the habitat in which the culture lives.
+//A more useful description: this is the hub in which the mobiles,
+//objects, and players all get moved around in a whirlwind of channel
+//dispatching and processing.
 package habitat
-
-/*
-Habitat - the habitat in which the culture lives.
-
-A more useful description: this is the hub in which the mobiles,
-objects, and players all get moved around in a whirlwind of channel
-dispatching and processing.
-*/
 
 import (
     "time"
@@ -32,6 +28,16 @@ func NewPool() Pool {
     return pool
 }
 
+// moveMobileTick waits N seconds and then calls itself again in a goroutine,
+// where N is a gaussian distributed number of "speed" mean and "speed * 0.1"
+// unit stddev.
+// The legacy approach to this was "every 2 seconds, loop through all mobiles
+// and do a computational thing all in one tight loop" which seems a little
+// choppy for a smooth flowing game. If there were two or more mobiles in the same
+// location, and multiple returned positive for the random movement test, any player
+// in that location would see all the mobiles leave at the exact same time. This
+// variance-adorned tick approach results in a smoother flow of movement among
+// the map.
 func moveMobileTick(mobile culture.Mobile, moveTick chan culture.Mobile) {
     moveTime := 200.0 / float64(mobile.Speed)
     moveTimeWithVariance := rand.NormFloat64() * (moveTime * 0.1) + moveTime
